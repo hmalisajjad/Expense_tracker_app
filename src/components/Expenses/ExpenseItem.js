fccb19ca76b2df5cdbc6598e7ad8d65417760f84
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2023-05-14 22:14:15 +0200

    change dynamic list data from static

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,18 @@
-import React, { useState } from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    console.log('ExpenseItem eveluated by React');
-
-    const clickHandler = () => {
-        setTitle('Updates!!');
-        console.log(title);
-    };
-    return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change title</button>
-        </Card>
-    );
-}
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
