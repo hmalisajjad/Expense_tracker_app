298f1265ba07d7c8672e2434441fab6e17487320
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2023-05-29 15:09:18 +0200

    Expense_app_completed

--- src/App.js
+++ src/App.js
@@ -12,24 +12,35 @@ const DUMMY_EXPENSES = [
   },
   {
     id: "e2",
+    title: "Mobile",
+    amount: 899.49,
+    date: new Date(2022, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Furniture",
+    amount: 1799.49,
+    date: new Date(2023, 5, 12),
+  },
+  {
+    id: "e4",
     title: "New TV",
     amount: 799.49,
     date: new Date(2021, 2, 12),
   },
   {
-    id: "e3",
+    id: "e5",
     title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: "e4",
+    id: "e6",
     title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
 ];
-
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
@@ -38,13 +49,6 @@ const App = () => {
       return [expense, ...prevExpenses];
     });
   };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, {items: expenses})
-  //  );
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
